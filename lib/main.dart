cc4a0217db08d703e400e27baa175b2d61da4279
kairatkubat <83951238+kairatkubat@users.noreply.github.com>
2023-02-09 14:44:54 +0600

    new budget app

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flu',
+      theme: ThemeData(
+       
+        primarySwatch: Colors.blue,
+      ),
+      home: const Shop(),
+    );
+  }
+}
+
+class Shop extends StatelessWidget {
+  const Shop({super.key,});
+  
+  @override
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+appBar: AppBar(actions: [], title: Text("Shop "),),
+    );
+
+   
+  }
+  }
