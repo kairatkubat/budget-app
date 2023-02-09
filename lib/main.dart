db9ac9e0f34ff4d997c0362595ed408110116107
kairatkubat <83951238+kairatkubat@users.noreply.github.com>
2023-02-09 16:11:12 +0600

    creating transaction file and work with it

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shop/transaction.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,21 +17,64 @@ class MyApp extends StatelessWidget {
        
         primarySwatch: Colors.blue,
       ),
-      home: const Shop(),
+      home: Shop(),
     );
   }
 }
 
 class Shop extends StatelessWidget {
-  const Shop({super.key,});
-  
-  @override
+  final List<Transaction> transactions = [
+    Transaction(id: "tq", title: "Shoes", amount: 78.8, date: DateTime.now()),
+    Transaction(id: "t2", title: "Grocery ", amount: 7238, date: DateTime.now()),
+
+  ].toList();
+   Shop({super.key,});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-appBar: AppBar(actions: [], title: Text("Shop "),),
+appBar: AppBar(actions: [], title: const Text("Shop "),),
+body: Column(
+  mainAxisAlignment: MainAxisAlignment.spaceAround  ,
+  crossAxisAlignment: CrossAxisAlignment.end,
+  children:[
+    Container(
+      width: double.infinity,
+      
+      child: const Card(
+        color: Colors.red,
+        child: Text("firs "),
+      ),
+    ),
+    Column(
+      children: transactions.map((e) {
+        return Card(
+          child: Row(
+            children: [
+              Container(
+                margin: EdgeInsets.all(20),
+                decoration: BoxDecoration(border: Border.all(
+                  color: Colors.blue,
+                  width: 2,
+                ),),
+                padding: EdgeInsets.all(10 ),
+                child: Text("\$ ${e.amount}", 
+                style: TextStyle(color: Colors.purple, fontSize: 20, fontWeight: FontWeight.bold ),),
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start ,
+                children: [
+                  Text(e.title , style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),),
+                  Text(e.date.toString(), style: TextStyle(color: Colors.grey),), 
+                ],
+              )
+            ],
+          ),
+        );
+      }).toList() ),
+    
+  ],
+)
     );
-
-   
-  }
-  }
+  }} 
